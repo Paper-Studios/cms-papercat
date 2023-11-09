ea52144e81bb912677417cfb7fde99cb17c539af
Rachel Miller <rkmiller131@gmail.com>
2023-11-09 14:30:07 -0800

    Added blog entry component, blog page route with an index slug for blog layout

---
+++ components/PostListItem.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Link from 'next/link'
+import ButtonX from '../react-bricks/bricks/layout/ButtonX'
+import dayjs from 'dayjs'
+import styles from '../css/Blog.module.css'
+
+interface PostListItemProps {
+  title: string
+  href: string
+  content: string
+  date: string
+  featuredImg?: string
+}
+
+const PostListItem: React.FC<PostListItemProps> = ({
+  title,
+  href,
+  content,
+  date,
+  featuredImg,
+}) => {
+  return (
+    <Link
+      href={`/blog/post/${href}`}
+      className={styles.blogEntry}
+    >
+      <img src={featuredImg} className={styles.blogImg} />
+
+      <div className={styles.contentContainer}>
+        <h2>{title}</h2>
+        <p>
+          {content.length + title.length > 250 ?
+            `${content.slice(0, (content.indexOf(' ', 225) - title.length))}...`
+            :
+            content
+          }
+        </p>
+      </div>
+
+      <div className={styles.moreInfoContainer}>
+        <span>{dayjs(date).format('MMM DD YYYY')}</span>
+        <ButtonX type="button" buttonType="link" href={`/blog/post/${href}`} size="small" form={false}/>
+      </div>
+    </Link>
+  )
+}
+
+export default PostListItem
