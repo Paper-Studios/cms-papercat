53e33116fe3139ca1089d49e1dcd502aeba777b8
Rachel Miller <rkmiller131@gmail.com>
2023-12-09 08:33:47 -0800

    Separation of concerns: put gameInfo into separate component, adjusted admin bug on Games.tsx

--- react-bricks/bricks/Games.tsx
+++ react-bricks/bricks/Games.tsx
@@ -1,51 +1,32 @@
-'use client'
 import React, { useState, useEffect, useContext } from 'react'
-import type { AppProps } from 'next/app'
 import { UseGameInfoContext, UseGameClickedContext } from './GameInfoContext'
-import { Text, RichText, Image, types, Repeater, useReactBricksContext, usePageValues } from 'react-bricks/frontend'
-import { PageViewer, usePage, cleanPage, ReactBricksContext } from 'react-bricks'
+import { Text, RichText, Image, types, Repeater, useAdminContext } from 'react-bricks/frontend'
 import Button from './layout/Button'
+import GameInfo from '../components/GameInfo'
 
 import styles from '../../css/Games.module.css'
 
 const Games: types.Brick<{}> = (props) => {
   const { gameInfo } = UseGameInfoContext();
   const {changeGame, gameDisplayed } = UseGameClickedContext();
-
-  // useEffect(() => {
-  //   console.log('game displayed is ', gameDisplayed);
-  // }, [gameDisplayed]);
-  console.log('gameDisplayed', gameDisplayed)
+  const { isAdmin, previewMode } = useAdminContext()
 
   function resetGameDisplayed (e) {
-    // console.log('e', e.taget.closest('.gameCard'))
-    console.log('e is ', e.target !== "Games_gameImg__pux6T");
-    // if (!e.target.closest('.gameCard')) {
-    //   console.log('firing')
-    //   changeGame('');
-    // }
+    if (!e.target.classList.contains('Games_gameImg__pux6T')) {
+      isAdmin && !previewMode ? '' :
+      changeGame('');
+    }
   }
+
+  console.log('game displayed is ', gameDisplayed);
 
   return (
     <div className={styles.gamesPage}>
       <div className={styles.gamesDisplayLeft}>
         <h3 style={{margin: '5% 0'}}>We make games!</h3>
-        {/* {gameDisplayed.title &&
-          <div className={styles.gameInfoDisplay}>
-            <div style={{marginBottom: '6%'}}>{gameDisplayed.title}</div>
-            <p style={{marginBottom: '6%', fontFamily: '"Source Sans 3", sans-serif'}}>{gameDisplayed.summary}</p>
-            <iframe
-              src={gameDisplayed.embeddedurl}
-              title="Paper Perjury Trailer"
-              frameBorder="0"
-              allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              className={styles.gameDisplayVideo}
-            />
-            <div className={styles.gamesButtonContainer}>
-              <Button icon={'steam'} text={'Buy on Steam'} href={gameDisplayed.steamLink} type={'button'} buttonType={'link'} size={'small'} form={false}/>
-            </div>
-          </div>
-        } */}
+        {gameDisplayed && Object.keys(gameDisplayed).length > 0 &&
+          <GameInfo />
+        }
       </div>
       <div className={styles.gamesSelectRight} onClick={(e) => resetGameDisplayed(e)}>
         <h2 style={{marginBottom: '5%'}}>Current releases:</h2>
@@ -67,16 +48,10 @@ Games.schema = {
     GameCard: [
       {
         name: 'Paper Perjury',
-        summary: 'TEST SUMMARY',
-        embeddedurl: 'www.youtube.com',
-        steamLink: 'www.webdevrachel.com',
+        summary: 'It all started with a simple robbery. When that thread was pulled, a tapestry of mysteries was revealed. Solve cases and outsmart criminals in a detective story full of lies, confessions, and maybe even a murder or two.',
+        embeddedurl: 'https://www.youtube.com/embed/mvKB5AkDQko?si=1ccbq7YIoceeRCtJ',
+        steamLink: 'https://store.steampowered.com/app/1919600/Paper_Perjury/',
       },
-      {
-        name: 'Second Game',
-        summary: 'Another Test summary for the second game to display for data sake',
-        embeddedurl: 'pretend I am a youtube url',
-        steamLink: 'pretend I am a steamlink url',
-      }
     ]
   })
 }
